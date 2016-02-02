e1ae482225a4d24c11b7273b91a0414620148d2c
Palmer Harrell <palmerharrell@me.com>
2016-02-02 10:28:45 -0600

    Copied instructions for exercise into js file

---
+++ javascripts/chaining.js
@@ -0,0 +1,8 @@
+// Using one single line of JavaScript code, complete the following tasks on the array of integers below.
+
+// Sort the numbers in descending order (10, 9, 8, 7, etc).
+// Remove any integers greater than 19.
+// Multiply each remaining number by 1.5 and then substract 1.
+// Then output (either in the DOM or the console) the sum of all the resulting numbers.
+// var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
