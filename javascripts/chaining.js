92b5818b7eb51c55f234b0ceb310873cd8cd2ae1
Palmer Harrell <palmerharrell@me.com>
2016-02-02 12:08:58 -0600

    Completed Chaining Array Methods exercise

--- javascripts/chaining.js
+++ javascripts/chaining.js
@@ -2,25 +2,14 @@
 
 // Sort the numbers in descending order (10, 9, 8, 7, etc). (SORT & REVERSE)
 // Remove any integers greater than 19. (FILTER)
-// Multiply each remaining number by 1.5 and then substract 1. (FOREACH)
+// Multiply each remaining number by 1.5 and then substract 1. (MAP)
 // Then output (either in the DOM or the console) the sum of all the resulting numbers. (REDUCE)
 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-console.log(integers.sort(function(a,b) {return a - b;}).reverse().filter(function(c) {return c <= 19}));
+// Sorted & reversed: 29, 25, 21, 20, 18, 13, 12, 11, 8, 7, 6, 3, 2, 1
+// Over 19 removed:   18, 13, 12, 11, 8, 7, 6, 3, 2, 1
+// Times 1.5, -1:     26, 18.5, 17, 15.5, 11, 9.5, 8, 3.5, 2, 0.5
+// Sum:               111.5
 
-
-// NOT CORRECT:
-// .forEach(function(d,e,f) {return (d * 1.5) - 1})
-
-
-// function logArrayElements(element, index, array) {
-//   console.log('a[' + index + '] = ' + element);
-// }
-
-// // Note elision, there is no member at 2 so it isn't visited
-// [2, 5, , 9].forEach(logArrayElements);
-// // logs:
-// // a[0] = 2
-// // a[1] = 5
-// // a[3] = 9
+console.log('Sorted, reversed, filtered, mapped & reduded: ',integers.sort(function(a,b) {return a - b;}).reverse().filter(function(c) {return c <= 19;}).map(function(d) {return (d*1.5)-1;}).reduce(function(e,f) {return e+f;}));
